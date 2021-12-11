0a51a604db441137c8bd85a9ef329e44c6c2fcd5
thavetta <43245361+thavetta@users.noreply.github.com>
2021-12-11 16:36:03 +0100

    Lab 8.2

---
+++ Laby/Lab08.2/RetezceSol/Retezce/Utils.cs
@@ -0,0 +1,122 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Retezce;
+
+public class Utils
+{
+    public static void VypisTyp(object prom)
+    {
+        Type type = prom.GetType();
+        Debug.WriteLine("Jmeno typu:" + type.FullName);
+        string hodnotovy = type.IsValueType ? "je hodnotovy" : "je referencni";
+        Debug.WriteLine("Typ " + hodnotovy);
+        Debug.WriteLine("Assembly:" + type.AssemblyQualifiedName);
+    }
+
+    public static string VratOtoceny(string vstup)
+    {
+        if (vstup == null)
+            return null;
+        if (string.IsNullOrEmpty(vstup))
+            return String.Empty;
+
+        StringBuilder sb = new StringBuilder(vstup.Length);
+
+        for (int i = vstup.Length - 1; i >= 0; i--)
+        {
+            sb.Append(vstup[i]);
+        }
+
+        return sb.ToString();
+    }
+
+    public static char[] NejvicVyskytu(string vstup)
+    {
+        if (string.IsNullOrEmpty(vstup))
+            return new char[0];
+
+        char[] znaky = new char[vstup.Length];
+        int[] pocet = new int[vstup.Length];
+        char[] vysledok;
+
+        int vkladam = 0;
+
+        foreach (char znak in vstup)
+        {
+            int index = NajdiZnak(znaky, znak);
+
+            if (index == -1)
+            {
+                znaky[vkladam] = znak;
+                pocet[vkladam] = 1;
+                vkladam++;
+            }
+            else
+            {
+                pocet[index]++;
+            }
+        }
+
+        int max = 0;
+
+        foreach (int item in pocet)
+        {
+            if (item > max)
+                max = item;
+        }
+
+        int pocetMax = 0;
+        foreach (int item in pocet)
+        {
+            if (item == max)
+                pocetMax++;
+        }
+
+        vysledok = new char[pocetMax];
+        int j = 0;
+        for (int i = 0; i < pocet.Length; i++)
+        {
+            if (pocet[i] == max)
+            {
+                vysledok[j] = znaky[i];
+                j++;
+            }
+        }
+
+        return vysledok;
+    }
+
+    private static int NajdiZnak(char[] znaky, char hledam)
+    {
+        for (int i = 0; i < znaky.Length; i++)
+        {
+            if (znaky[i] == hledam)
+                return i;
+        }
+
+        return -1;
+    }
+
+    public static char[] NejvicVyskytuLinq(string vstup)
+    {
+        if (string.IsNullOrEmpty(vstup))
+            return new char[0];
+
+        Dictionary<char, int> data = new Dictionary<char, int>();
+
+        foreach (var item in vstup)
+        {
+            if (data.ContainsKey(item))
+                data[item]++;
+            else
+                data.Add(item, 1);
+        }
+
+        int max = data.Values.Max();
+
+        char[] vysledek = data.Where(x => x.Value == max).Select(x => x.Key).ToArray();
+
+        return vysledek;
+    }
+}
---
+++ Laby/Lab08.2/RetezceSol/RetezceTests/UtilsTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Retezce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retezce.Tests
+{
+    [TestClass()]
+    public class UtilsTests
+    {
+        [TestMethod()]
+        public void VratOtocenyTest()
+        {
+            string vstup = "pokus";
+            string ocekavam = "sukop";
+            Utils.VypisTyp(vstup);
+
+            string vysledek = Utils.VratOtoceny(vstup);
+            Assert.AreEqual(ocekavam, vysledek);
+        }
+
+        [TestMethod()]
+        public void NejvicVyskytuTest1()
+        {
+            string vstup = "pokus";
+            char[] ocekavam = { 'p','o','k','u','s' };
+            char[] vysledek = Utils.NejvicVyskytu(vstup);
+            Assert.AreEqual(ocekavam[0], vysledek[0]);
+            Assert.AreEqual(ocekavam[1], vysledek[1]);
+            Assert.AreEqual(ocekavam[2], vysledek[2]);
+            Assert.AreEqual(ocekavam[3], vysledek[3]);
+            Assert.AreEqual(ocekavam[4], vysledek[4]);
+        }
+
+        [TestMethod()]
+        public void NejvicVyskytuTest2()
+        {
+            string vstup = "mama ma male boty";
+            char[] ocekavam = { 'm', 'a' };
+            char[] vysledek = Utils.NejvicVyskytu(vstup);
+            Assert.AreEqual(ocekavam[0], vysledek[0]);
+            Assert.AreEqual(ocekavam[1], vysledek[1]);
+            Assert.AreEqual(ocekavam.Length, vysledek.Length);
+        }
+    }
+}
