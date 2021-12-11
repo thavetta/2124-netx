ecbfcbef7a26130393a78b2e9cf89100c638ad2f
thavetta <43245361+thavetta@users.noreply.github.com>
2021-12-11 12:33:19 +0100

    Lab 4.2

---
+++ Laby/Lab04.2/Matika1Sol/Matika1/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a;
+int b;
+
+while (true)
+{
+    Console.WriteLine("Zadejte číslo a:");
+    try
+    {
+        a = int.Parse(Console.ReadLine());
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Vstup obsahuje neplatné znaky");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Vstup je příliš velké číslo");
+    }
+}
+
+while (true)
+{
+    Console.WriteLine("Zadejte číslo b:");
+    try
+    {
+        b = int.Parse(Console.ReadLine());
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Vstup obsahuje neplatné znaky");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Vstup je příliš velké číslo");
+    }
+}
+
+int x = a;
+int y = b;
+
+//Algoritmus NSD
+while (x != y)
+    if (x > y)
+        x -= y;
+    else
+        y -= x;
+
+Console.WriteLine("NSD čísel {0} a {1} je {2}", a, b, x);
+
+Console.WriteLine($"NSN čísel {a} a {b} je {a * b / x}");
+
