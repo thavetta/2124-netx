37255790d2cc5813f0cb7cd3ab1ae445e215de13
thavetta <43245361+thavetta@users.noreply.github.com>
2021-12-11 14:05:05 +0100

    Lab 7

---
+++ Laby/Lab07/BankaSol/Banka/UcetZaklad.cs
@@ -0,0 +1,53 @@
+﻿namespace Banka;
+
+public class UcetZaklad
+{
+    private decimal stav;
+    private string majitel;
+    private StatusUctu status = StatusUctu.Aktivny;
+
+    public void NastavMajitele(string majitel)
+    {
+        if (string.IsNullOrWhiteSpace(majitel))
+            throw new ArgumentException("Jmeno majitele musí obsahovat viditelné znaky");
+
+        this.majitel = majitel;
+    }
+
+    public string VratMajitele()
+    {
+        return this.majitel;
+    }
+
+    public decimal VratStav()
+    {
+        return this.stav;
+    }
+
+    public void Vklad(decimal castka)
+    {
+        if (this.status != StatusUctu.Aktivny)
+            throw new InvalidOperationException("Operacia na neaktivnom ucte");
+
+        this.stav += castka;
+        Console.WriteLine("Na účet majitele {0} bylo vloženo {1} a stav je {2}", this.majitel, castka, this.stav);
+    }
+
+    public void Vyber(decimal castka)
+    {
+        if (this.status != StatusUctu.Aktivny)
+            throw new InvalidOperationException("Operace na neaktím účtu není povolena");
+
+        if (this.stav < castka)
+            throw new InvalidOperationException("Nedostatečné krytí na účtu");
+
+        this.stav -= castka;
+        Console.WriteLine("Z účtu majitele {0} bylo vybráno {1} a stav je {2}",
+            this.majitel, castka, this.stav);
+    }
+
+    public override string ToString()
+    {
+        return majitel + " - " + stav;
+    }
+}
---
+++ Laby/Lab07/BankaSol/TestBanky/Program.cs
@@ -0,0 +1,17 @@
+﻿using Banka;
+// See https://aka.ms/new-console-template for more information
+UcetZaklad ucetA = new UcetZaklad();
+UcetZaklad ucetB = new UcetZaklad();
+
+ucetA.NastavMajitele("Tomas");
+ucetA.NastavMajitele("Eva");
+
+ucetA.Vklad(1000);
+ucetB.Vklad(500);
+
+ucetA.Vyber(500);
+ucetB.Vyber(100);
+
+Console.WriteLine(ucetA);
+
+ucetA.Vyber(1200);
