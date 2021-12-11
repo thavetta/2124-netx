5898bf5339bebc0fcf5cae75703335bc08de9ba6
thavetta <43245361+thavetta@users.noreply.github.com>
2021-12-11 12:54:13 +0100

    Lab 5

---
+++ Laby/Lab05/CislaSol/Cisla/Pocitadlo.cs
@@ -0,0 +1,80 @@
+﻿namespace Cisla;
+
+public class Pocitadlo
+{
+    public static int NactiCislo(string text = "Zadej číslo:")
+    {
+        int cislo;
+        while (true)
+        {
+            Console.WriteLine(text);
+            if (int.TryParse(Console.ReadLine(), out cislo))
+                break;
+            Console.WriteLine("Nekorektně zadané číslo / opakujte");
+
+        }
+        return cislo;
+    }
+
+    public static int NSD(int x, int y)
+    {
+        if (x < 1 || y < 1)
+            throw new ArgumentOutOfRangeException("Vstup musí být kladný větší než 0");
+
+        while (x != y)
+        {
+            if (x > y)
+            {
+                x -= y;
+            }
+            else
+            {
+                y -= x;
+            }
+        }
+        return x;
+    }
+
+    public static int NSN(int x, int y)
+    {
+        return x * y / NSD(x, y);
+    }
+
+    public static int Faktorial(int x)
+    {
+        if (x == 1)
+            return 1;
+
+        return x * Faktorial(x - 1);
+    }
+
+    public static bool JePrvocislo(int x)
+    {
+        // Pro čísla menší než 2 nejde vůbec mluvit o prvočíslech
+        if (x < 2)
+            return false;
+
+        //Dvojka je jediné sudé prvočíslo
+        if (x == 2)
+            return true;
+
+        //Pokud je bit 0 rovný nule, je číslo sudé a tedy není prvočíslo (dvojku vyřadil předešlý test) 
+        if ((x & 1) == 0)
+            return false;
+
+        //Dělite má smysl hledat jen do odmocniny z testovaného čísla
+        int limit = (int)Math.Sqrt(x) + 1;
+
+        //Nemá smysl dělit sudým číslem, sudé číslo krát cokoliv je sudé číslo a my máme teď jasno, že naše číslo je liché.
+        for (int delic = 3; delic < limit; delic += 2)
+        {
+            //Pokud najdu dělitele, je číslo prvočíslo
+            if ((x % delic) == 0)
+                return false;
+        }
+
+        //Pokud jsem nenašel dělitele, je číslo prvočíslo
+        return true;
+
+    }
+}
---
+++ Laby/Lab05/CislaSol/Matika2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Cisla;
+// See https://aka.ms/new-console-template for more information
+
+int a = Pocitadlo.NactiCislo("Zadej cislo a:");
+int b = Pocitadlo.NactiCislo("Zadej cislo b:");
+
+int nsd = Pocitadlo.NSD(a, b);
+int nsn = Pocitadlo.NSN(a, b);
+
+Console.WriteLine($"NSD({a},{b}) = {nsd}");
+Console.WriteLine($"NSN({a},{b}) = {nsn}");
+
+int x = Pocitadlo.NactiCislo("Zadej číslo pro test prvočísla:");
+
+string vyrok = Pocitadlo.JePrvocislo(x) ? "je" : "není";
+
+Console.WriteLine($"Číslo {x} {vyrok} prvočíslo");
