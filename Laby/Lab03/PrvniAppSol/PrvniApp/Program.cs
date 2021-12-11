66181ef134a112ebf71d37694d598e0a8f141236
thavetta <43245361+thavetta@users.noreply.github.com>
2021-12-11 11:59:15 +0100

    Lab 3

---
+++ Laby/Lab03/PrvniAppSol/PrvniApp/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 10;
+int b = 3;
+
+// Načtení od uživatele (Extra zadání)
+//Console.WriteLine("Zadejte číslo a:");
+//a = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Zadejte číslo b:");
+//b = int.Parse(Console.ReadLine());
+
+Console.Write("Součet: ");
+Console.WriteLine(a + b);
+
+Console.Write("Rozdíl: ");
+Console.WriteLine(a - b);
+
+Console.Write("Nasobení: ");
+Console.WriteLine(a * b);
+
+Console.Write("Dělení celočíselné: ");
+Console.WriteLine(a / b);
+
+Console.Write("Dělení v reálných číslech: ");
+Console.WriteLine(a / (double)b);
+
+Console.Write("Zbytek: ");
+Console.WriteLine(a % b);
+
+Console.Write("Posun doprava: ");
+Console.WriteLine(a >> b);
+
+Console.Write("Posun doleva: ");
+Console.WriteLine(a << b);
