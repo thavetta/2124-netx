a9f8d119372eb67095178feca5174a6878714761
thavetta <43245361+thavetta@users.noreply.github.com>
2021-12-11 12:11:20 +0100

    Lab 4

---
+++ Laby/Lab04.1/Matika1Sol/Matika1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Zadejte číslo a:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Zadejte číslo b:");
+int b = int.Parse(Console.ReadLine());
+
+int x = a;
+int y = b;
+
+//Algoritmus NSD
+while (x != y)
+    if (x > y)
+        x -= y;
+    else
+        y -= x;
+
+Console.WriteLine("NSD čísel {0} a {1} je {2}",a,b,x);
+
+Console.WriteLine($"NSN čísel {a} a {b} je {a*b/x}");
+
