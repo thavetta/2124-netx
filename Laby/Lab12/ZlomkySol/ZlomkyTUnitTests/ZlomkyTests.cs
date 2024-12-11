d90df496ea65522a3cef97af1c6e3109b54b8fa6
Tomas Havetta <thavetta@ccn.sk>
2024-12-11 19:16:46 +0100

    Pridan TUnit test

---
+++ Laby/Lab12/ZlomkySol/ZlomkyTUnitTests/ZlomkyTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zlomky;
+using TUnit.Core;
+
+namespace ZlomkyTUnitTests;
+
+public class ZlomkyTests
+{
+        [Test]
+        public async Task Konstruktor_Korektni_VytvoriZlomek()
+        {
+            Zlomek z = new Zlomek(4, 8);
+            await Assert.That(z.Citatel).IsEqualTo(1);
+            await Assert.That(z.Jmenovatel).IsEqualTo(2);
+        }
+}
